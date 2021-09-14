b7d43147899a2ae2ae43aa299a73598918b4a6df
PrFullStack <89317653+PrFullStack@users.noreply.github.com>
2021-09-14 20:05:07 +0100

    Update Computer.js

--- backend/Models/Computer.js
+++ backend/Models/Computer.js
@@ -3,6 +3,6 @@ const mongoose =require('mongoose')
 const Product =require('./Product')
 
 const options = { discriminatorKey: 'categorie' };
-const Computer =Product.discriminator('Computer',new mongoose.Schema({capacity:Number},options))
+const Computer =Product.discriminator('Computer',new mongoose.Schema({capacity:Number,os:String,screen:String,memory:String,processor:String},options))
 
 module.exports= Computer;
