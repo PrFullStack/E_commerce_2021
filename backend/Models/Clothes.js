0bc26ae4d72e3f0aa712cfbeea62df0add7af25e
PrFullStack <89317653+PrFullStack@users.noreply.github.com>
2021-09-14 20:05:27 +0100

    Update Clothes.js

--- backend/Models/Clothes.js
+++ backend/Models/Clothes.js
@@ -3,6 +3,6 @@ const mongoose =require('mongoose')
 const Product =require('./Product')
 
 const options = { discriminatorKey: 'categorie' };
-const Clothes =Product.discriminator('Clothes',new mongoose.Schema({color:String,size:String},options))
+const Clothes =Product.discriminator('Clothes',new mongoose.Schema({size:String},options))
 
 module.exports= Clothes;
