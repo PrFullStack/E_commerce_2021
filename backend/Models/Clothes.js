19845d88f8f58e7784c85c52f5dca3b347c32b4c
PrFullStack <89317653+PrFullStack@users.noreply.github.com>
2021-09-15 16:08:42 +0100

    Delete backend/Models directory

--- backend/Models/Clothes.js
+++
@@ -1,8 +0,0 @@
-const mongoose =require('mongoose')
-
-const Product =require('./Product')
-
-const options = { discriminatorKey: 'categorie' };
-const Clothes =Product.discriminator('Clothes',new mongoose.Schema({size:String},options))
-
-module.exports= Clothes;
