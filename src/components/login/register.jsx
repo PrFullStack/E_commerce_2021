5a8564c227b0c3b1c961cba729f376ca2c299bd8
ayoub-kabir <kabir.ayoub0@gmail.com>
2021-09-29 19:03:59 +0100

    Login et Register Page

---
+++ src/components/login/register.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import loginImg from "../../login.svg";
+
+export class Register extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+//              <label htmlFor="username">Username</label>
+  render() {
+    return (
+      
+      <div className="base-container" ref={this.props.containerRef}>
+        <div className="header">Register</div>
+        <div className="content">
+          <div className="image">
+            <img src={loginImg} />
+          </div>
+          <div className="form">
+            <div className="form-group">
+              <div className="username">
+              <i class="fas fa-user"></i>
+              &ensp; &ensp;
+              <input type="text" name="First name" placeholder="First name" />
+              </div>
+              
+            </div>
+            <div className="form-group">
+            <div className="username">
+            <i class="fas fa-user"></i>
+              &ensp; &ensp;
+              <input type="text" name="First name" placeholder="Last name" />
+              </div>
+            </div>
+            <div className="form-group">
+            <div className="username">
+            <i class="fas fa-phone"></i>
+              &ensp; &ensp;
+              <input type="text" name="First name" placeholder="Phone number" />
+              </div>
+            </div>
+            <div className="form-group">
+            <div className="username">
+            <i class="fas fa-envelope-open"></i>
+              &ensp; &ensp;
+              <input type="text" name="First name" placeholder="Email" />
+              </div>
+            </div>
+            <div className="form-group">
+            <div className="username">
+            <i class="fas fa-key"></i>
+              &ensp; &ensp;
+              <input type="text" name="First name" placeholder="Password" />
+              </div>
+            </div>
+          </div>
+          <div className="footer">
+        <div className="register">
+            <button type="button" className="btn">
+
+            <i class="fas fa-user-plus"></i>
+            &ensp;
+            Register
+            </button>
+          </div>
+        </div>
+        </div>
+        
+      </div>
+    );
+  }
+}
