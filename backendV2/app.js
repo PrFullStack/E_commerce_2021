8bb62d75a9c0b548cf31797ff2e0c83d6909e9c2
PrFullStack <89317653+PrFullStack@users.noreply.github.com>
2021-10-02 17:26:47 +0100

    Add files via upload

---
+++ backendV2/app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const mongoose= require('mongoose')//odm
+require('dotenv/config');
+
+const cors = require('cors')
+
+// connect to mongodb 
+app.use(cors())
+const connect =async ()=>{
+await mongoose.connect("mongodb+srv://ADMIN:St0770lb0770@cluster0.ri6ni.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+{useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology: true})
+console.log('db connected')
+};
+
+
+app.use(express.static(__dirname + '/assets'));
+connect();
+/// instead of body-parser
+app.use(express.json())
+
+//use route products when route is like /products
+const ProductRoute=require('./Routes/Product')
+app.use('/Products',ProductRoute)
+
+const ClientRoute=require('./Routes/Client')
+app.use('/Clients',ClientRoute)
+
+
+
+
+//start listening 
+app.listen(9393, () => {
+  console.log("started");
+})
